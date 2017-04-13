724c6afa2373e10a98af888ba6fe01de139816ea
Flora Harvey <fharvey555@gmail.com>
2017-04-13 16:06:42 +0100

    Add tests for saving colour and lineWidth of coordinates to model

--- spec/DrawingSpec.js
+++ spec/DrawingSpec.js
@@ -6,7 +6,7 @@
 
    beforeEach(function() {
      drawing = new Drawing();
-     drawing.addCoordinates(1,2);
+     drawing.addCoordinates(1,2, '#adadad', 10);
    });
 
    it("should create and save coordinates", function() {
@@ -18,5 +18,11 @@
      drawing.addCoordinates(150,200);
      expect(drawing.returnLastCoordinates().x).toEqual(150);
      expect(drawing.returnLastCoordinates().y).toEqual(200);
-   })
- })
+   });
+
+   it("should save colour and tool size for each saved coordinates", function() {
+     expect(drawing.coordinates[0].colour).toEqual('#adadad');
+     expect(drawing.coordinates[0].toolSize).toEqual(10);
+   });
+
+ });
